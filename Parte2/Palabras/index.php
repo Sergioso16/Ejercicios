ed0b9bcadc5636426164402aad8dbd3081c69642
Sergioso16 <126101030+Sergioso16@users.noreply.github.com>
2023-02-27 12:31:41 -0600

    Update index.php

--- Parte2/Palabras/index.php
+++ Parte2/Palabras/index.php
@@ -14,29 +14,29 @@
 
   <?php
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recupera el valor del campo de entrada del formulario
+    
     $palabra = $_REQUEST['palabra'];
 
-    // Inicializa los contadores de vocales y consonantes en cero
+    
     $num_vocales = 0;
     $num_consonantes = 0;
 
-    // Itera sobre cada letra de la palabra
+    
     for ($i = 0; $i < strlen($palabra); $i++) {
       $letra = strtolower($palabra[$i]); // Convierte la letra a minúscula para comparar con las vocales
 
-      // Si es una vocal, incrementa el contador de vocales
+      
       if ($letra == 'a' || $letra == 'e' || $letra == 'i' || $letra == 'o' || $letra == 'u') {
         $num_vocales++;
       }
 
-      // Si es una consonante, incrementa el contador de consonantes
+      
       else if (preg_match('/[a-z]/', $letra)) {
         $num_consonantes++;
       }
     }
 
-    // Muestra los resultados en la página
+    
     echo "<p>La palabra '" . $palabra . "' tiene " . $num_vocales . " vocales y " . $num_consonantes . " consonantes.</p>";
   }
   ?>
