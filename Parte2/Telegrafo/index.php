ad69e06a2f80bf70822bde4544db859ff0ffa3a6
Sergioso16 <126101030+Sergioso16@users.noreply.github.com>
2023-02-27 12:33:18 -0600

    Update index.php

--- Parte2/Telegrafo/index.php
+++ Parte2/Telegrafo/index.php
@@ -14,38 +14,38 @@
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Recupera el valor del campo de entrada del formulario
+  
   $mensaje = $_REQUEST['mensaje'];
 
-  // Inicializa el costo en cero
+ 
   $costo = 0;
 
   // Itera sobre cada caracter del mensaje
   for ($i = 0; $i < strlen($mensaje); $i++) {
     $caracter = $mensaje[$i];
 
-     // Si es un espacio, no hace nada
+     
      if ($caracter == ' ') {
         continue;
       }
 
-    // Si es una letra, suma $10 al costo
+    
     if (ctype_alpha($caracter)) {
       $costo += 10;
     }
 
-    // Si es un dígito, suma $20 al costo
+    
     else if (ctype_digit($caracter)) {
       $costo += 20;
     }
 
-    // Si es un caracter especial, suma $30 al costo
+    
     else {
       $costo += 30;
     }
   }
 
-  // Muestra el costo del mensaje en la página
+  
   echo "El costo del mensaje es: $" . $costo;
 }
 ?>
